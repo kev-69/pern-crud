a26dc687a9a8e796db849dce69297afd10a5c957
kev-69 <bismarkobuobi19@gmail.com>
2025-03-21 18:04:17 +0000

    Add user routes for CRUD operations in userRoutes.js

---
+++ src/routes/userRoutes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+
+const { 
+    getAllUsers, 
+    getUserById, 
+    createUser, 
+    updateUser, 
+    deleteUser 
+} = require("../controllers/userController");
+
+router.get("/users", getAllUsers);
+router.get("/users/:id", getUserById);
+router.post("/users", createUser);
+router.put("/users/:id", updateUser);
+router.delete("/users/:id", deleteUser);
+
+
+module.exports = router;
+// The code above is a route file for the user routes. It uses the express package to create a router. The router is then used to define the routes for getting all users, getting a user by id, creating a user, updating a user, and deleting a user. The router is then exported for use in other parts of the application. The router is used in the server.js file to define the user routes for the application.
